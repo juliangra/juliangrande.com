7a7e7065845e03a33beaef7f2b80a4f38cf6249c
Julian <juliangr@stud.ntnu.no>
2023-09-23 17:37:07 +0200

    feat: add meta information for SEO purposes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,17 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
+  app: {
+    head: {
+      title: 'Julian Grande - Software Engineer',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'I am a student at the Norwegian University of Science and Technology, pursuing a masters degree in computer science. I also work as a research assistant at NTNU, and as a software engineer specializing in full stack development.' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  }
 });
