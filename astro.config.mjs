adc74241b2abe623a79531ac91b996af434ee52a
Julian Grande <julian.grande@bekk.no>
2026-03-01 13:05:54 +0100

    feat: added illustrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://juliangrande.com',
@@ -14,6 +15,7 @@ export default defineConfig({
       },
     }),
     sitemap(),
+    icon(),
   ],
   vite: {
     plugins: [tailwindcss()],
